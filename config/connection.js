23952838a3c990055dd8a6c7ed1e43e401f4035b
dianesabra <dwsabra@gmail.com>
2019-03-14 18:14:41 -0500

    push changes

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Khara6290",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Khara6290",
+//   database: "burgers_db"
+// });
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(function(err) {
   if (err) {
